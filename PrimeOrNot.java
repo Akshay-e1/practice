eee9b21feaf6f10b541bccb196f874075139e6df
CHITITHOTI AKSHAYNATH <akshaynathcse029@gmail.com>
2025-11-13 22:28:57 +0530

    Update PrimeOrNot.java

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -1,14 +1,22 @@
 public class PrimeOrNot {
-    public static void main(String args[])
-    {
-        int n=6;
-        if(n%2==0)
-        {
+    public static void main(String args[]) {
+        int n = 6;
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false; // 0 and 1 are not prime
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
             System.out.println("prime");
-        }
         else
-        {
             System.out.println("not prime");
-        }
     }
 }
